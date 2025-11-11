82fd4b1cc519f0d8c40e62b1e0a6162c3d7148ec
ziadasar <ziadasar@gmail.com>
2025-11-11 02:31:34 +0200

    DQN working

--- assignment2.py
+++ assignment2.py
@@ -1,2 +1,6 @@
-import torch
-print(torch.version.cuda)
+import wandb
+
+wandb.login()  # enter your API key here
+wandb.init(project="my-test-run")
+for x in range(10):
+    wandb.log({"loss": 10 - x})
